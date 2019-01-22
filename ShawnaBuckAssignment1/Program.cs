852b79ee6abeaee4a6ac9fd6aec410d4de77e57d
Shawna <sortegon@mail.usf.edu>
2019-01-21 22:05:19 -0500

    Assignment 1, part 1: isPrime

---
+++ ShawnaBuckAssignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShawnaBuckAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
